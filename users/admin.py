d46d3f167a357a39f9a9232af5703db22aa566af
nimagolkhanban <ningoban@gmail.com>
2023-08-15 19:23:52 +0330

    fix : AuthorAdminSite

--- users/admin.py
+++ users/admin.py
@@ -7,4 +7,15 @@ from django.contrib.admin import AdminSite
 admin.site.register(Author)
 
 
+class AuthorAdminSite(AdminSite):
+    site_header = "Author Admin"
 
+
+author_admin_site = AuthorAdminSite(name="author_admin")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["name", "biography"]
+
+
+author_admin_site.register(Author, AuthorAdmin)
