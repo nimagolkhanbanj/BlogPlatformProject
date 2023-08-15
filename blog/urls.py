11e66c799c3d637533794d542e4fc88423c930a7
nimagolkhanban <ningoban@gmail.com>
2023-08-15 19:22:07 +0330

    add : fix blog url file

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from .views import home, post_list,post_details,category_details,category_list, comment_update
+from .views import *
 
-app_name='blog'
+app_name = 'blog'
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('post/', post_list, name="post_list"),
-    path('post/<int:pk>/', post_details, name="post_details"),
-    path('categories/', category_list, name="category_list"),
-    path('categories/<int:pk>/', category_details, name="category_details"),
-    path('comment_update/<int:pk>',comment_update,name='comment_update'),
-
+    path("", home, name="home"),
+    path("post/", PostListView.as_view(), name="post_list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_details"),
+    path("categories/", category_list, name="category_list"),
+    path("categories/<int:pk>/", category_details, name="category_details"),
+    path("comment_update/<int:pk>", CommentUpdateView.as_view(), name="comment_update"),
 ]
